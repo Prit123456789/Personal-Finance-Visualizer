fdf747e4ed702b87293a52aa020d1288033908e7
Prit patel <148608375+Prit123456789@users.noreply.github.com>
2025-03-07 21:35:25 +0530

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,3 +1,4 @@
+// theme-provider.tsx
 'use client'
 
 import * as React from 'react'
@@ -5,7 +6,21 @@ import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
 } from 'next-themes'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <AnimatePresence mode="wait">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </NextThemesProvider>
+  )
 }
